47cf52c9dbfc6d77792ffab4bb22c5d46218d924
Lee Macri <lem@rathkeale.school.nz>
2019-05-22 09:37:03 +1200

    Initial Files Upload

---
+++ AS91906/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AS91906")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("AS91906")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1df1c6b4-6ca8-42c6-9610-b415de9419f0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ AS91906/frmMain.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+'''     
+''' </summary>
+''' 
+''' <author>
+''' 
+''' </author>
+''' 
+''' <date>
+''' 
+''' </date>
+''' 
+Public Class frmMain
+
+End Class
